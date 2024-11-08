4d80033d1e0c1c125a7445ba173582e41a8c690f
kwited <blockerglocker@gmail.com>
2024-11-08 18:32:00 +0300

    video 9-10

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -9,13 +9,13 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class Player extends Entity {
+public class Player extends Entity{
+
     GamePanel gp;
     KeyHandler keyH;
-
     public final int screenX;
     public final int screenY;
-    int hasKey = 0;
+    public int hasKey = 0;
 
     public Player(GamePanel gp, KeyHandler keyH){
         this.gp = gp;
@@ -35,7 +35,9 @@ public class Player extends Entity {
         setDefaultValues();
         getPlayerImage();
     }
+
     public void setDefaultValues(){
+
         worldX = gp.tileSize * 23;
         worldY = gp.tileSize * 21;
         speed = 4;
@@ -43,6 +45,7 @@ public class Player extends Entity {
     }
     public void getPlayerImage(){
         try{
+
             up1 = ImageIO.read(new File("res/player/boy_up_1.png"));
             up2 = ImageIO.read(new File("res/player/boy_up_2.png"));
             down1 = ImageIO.read(new File("res/player/boy_down_1.png"));
@@ -52,20 +55,27 @@ public class Player extends Entity {
             right1 = ImageIO.read(new File("res/player/boy_right_1.png"));
             right2 = ImageIO.read(new File("res/player/boy_right_2.png"));
 
-        }catch (IOException e){
+        }catch(IOException e){
             e.printStackTrace();
         }
     }
 
+
     public void update(){
-        if (keyH.downPressed == true || keyH.upPressed == true || keyH.leftPressed == true || keyH.rightPressed == true){
-            if(keyH.upPressed == true){
+
+        if(keyH.upPressed == true || keyH.downPressed == true ||
+                keyH.leftPressed == true || keyH.rightPressed == true){
+
+            if (keyH.upPressed == true) {
                 direction = "up";
-            }else if(keyH.downPressed == true){
+            }
+            else if (keyH.downPressed == true) {
                 direction = "down";
-            }else if(keyH.leftPressed == true){
+            }
+            else if (keyH.leftPressed == true) {
                 direction = "left";
-            }else if(keyH.rightPressed == true){
+            }
+            else if (keyH.rightPressed == true) {
                 direction = "right";
             }
 
@@ -73,95 +83,114 @@ public class Player extends Entity {
             //CHECK TILE COLLISION
             collisionOn = false;
             gp.cChecker.checkTile(this);
+
             // CHECK OBJECT COLLISION
-            int objIndex  = gp.cChecker.checkObject(this,true);
+            int objIndex = gp.cChecker.checkObject(this, true);
             pickUpObject(objIndex);
-            // IF COLLISION FALSE, PLAYER CAN MOVE
-            if(collisionOn == false){
-                switch (direction){
-                    case "up":
-                        worldY -= speed;
-                        break;
-                    case "down":
-                        worldY += speed;
-                        break;
-                    case "left":
-                        worldX -= speed;
-                        break;
-                    case "right":
-                        worldX += speed;
-                        break;
+
+            // IF COLLISION IS FALSE, PLAYER CAN MOVE
+            if(collisionOn == false) {
+
+                switch(direction) {
+                    case "up": worldY -= speed; break;
+                    case "down": worldY += speed; break;
+                    case "left": worldX -= speed; break;
+                    case "right": worldX += speed; break;
                 }
             }
 
             spriteCounter++;
             if(spriteCounter > 12){
-                if (spriteNum == 1){
+                if(spriteNum == 1){
                     spriteNum = 2;
-                }else if (spriteNum == 2){
+                }
+                else if(spriteNum == 2){
                     spriteNum = 1;
                 }
                 spriteCounter = 0;
             }
         }
-
     }
-    public void pickUpObject(int i){
-        if(i != 999){
+
+    public void pickUpObject(int i) {
+
+        if(i != 999) {
+
             String objectName = gp.obj[i].name;
-            switch (objectName){
+
+            switch(objectName) {
                 case "Key":
+                    gp.playSE(1);
                     hasKey++;
                     gp.obj[i] = null;
-                    System.out.println("Key:" + hasKey);
+                    gp.ui.showMessage("You got a key!");
                     break;
                 case "Door":
-                    if(hasKey > 0){
+                    if(hasKey > 0)	{
+                        gp.playSE(3);
                         gp.obj[i] = null;
                         hasKey--;
+                        gp.ui.showMessage("You opened the door!");
                     }
-                    System.out.println("Key:" + hasKey);
+                    else {
+                        gp.ui.showMessage("You need a key!");
+                    }
+                    System.out.println("Key:"+hasKey);
+                    break;
+                case "Boots":
+                    gp.playSE(2);
+                    speed += 2;
+                    gp.obj[i] = null;
+                    gp.ui.showMessage("Speed up!");
+                    break;
+                case "Chest":
+                    gp.ui.gameFinished = true;
+                    gp.stopMusic();
+                    gp.playSE(4);
                     break;
             }
         }
     }
+
     public void draw(Graphics2D g2){
+
         BufferedImage image = null;
-        switch (direction){
+
+        switch(direction){
             case "up":
-                if (spriteNum == 1){
+                if(spriteNum == 1){
                     image = up1;
                 }
-                if (spriteNum == 2){
+                if(spriteNum == 2){
                     image = up2;
                 }
                 break;
             case "down":
-                if (spriteNum == 1){
+                if(spriteNum == 1){
                     image = down1;
                 }
-                if (spriteNum == 2){
+                if(spriteNum == 2){
                     image = down2;
                 }
                 break;
             case "left":
-                if (spriteNum == 1){
+                if(spriteNum == 1){
                     image = left1;
                 }
-                if (spriteNum == 2){
+                if(spriteNum == 2){
                     image = left2;
                 }
                 break;
             case "right":
-                if (spriteNum == 1){
+                if(spriteNum == 1){
                     image = right1;
                 }
-                if (spriteNum == 2){
-                    image = right2;
+                if(spriteNum == 2){
+                    image = right2;;
                 }
+
                 break;
         }
-        g2.drawImage(image, screenX,screenY,gp.tileSize,gp.tileSize, null);
+        g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
     }
-
 }
--- src/game/GamePanel.java
+++ src/game/GamePanel.java
@@ -4,109 +4,136 @@ import entity.Player;
 import object.SuperObject;
 import tile.TileManager;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JPanel;
 
-public class GamePanel extends JPanel implements Runnable{
-    final int originalTileSize = 16; // 16x16
+public class GamePanel extends JPanel implements Runnable {
+
+    final int originalTileSize = 16;
     final int scale = 3;
 
-    public final int tileSize = originalTileSize * scale; // 48x48
+    public final int tileSize = originalTileSize * scale;
     public final int maxScreenCol = 16;
     public final int maxScreenRow = 12;
-    public final int screenWidth = tileSize * maxScreenCol; // 768 pixels
-    public final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+    public final int screenWidth = tileSize * maxScreenCol;
+    public final int screenHeight = tileSize * maxScreenRow;
 
+    //WORLD SETTINGS
 
-    //World Settings
     public final int maxWorldCol = 50;
     public final int maxWorldRow = 50;
-    public final int worldWidth = tileSize / maxWorldCol;
-    public final int worldHeight = tileSize / maxWorldRow;
 
     int FPS = 60;
 
+    // SYSTEM
     TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
-    Thread gameThread;
+    Sound music = new Sound();
+    Sound se = new Sound();
     public CollisionChecker cChecker = new CollisionChecker(this);
     public AssetSetter aSetter = new AssetSetter(this);
-    public Player player = new Player(this,keyH);
+    public UI ui = new UI(this);
+    Thread gameThread;
+
+    // ENTITY AND OBJECT
+    public Player player = new Player(this, keyH);
     public SuperObject obj[] = new SuperObject[10];
 
 
-    // Set default position player's
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 4 ;
+    public GamePanel(){
 
-    public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
-        this.setBackground(Color.BLACK);
+        this.setBackground(Color.black);
         this.setDoubleBuffered(true);
         this.addKeyListener(keyH);
         this.setFocusable(true);
+
     }
 
-    public void setupGame(){
+    public void setupGame()	{
         aSetter.setObject();
+
+        playMusic(0);
     }
 
-    public void startGameThread(){
+    public void startGameThread() {
+
         gameThread = new Thread(this);
         gameThread.start();
     }
 
     @Override
-    public void run() {
+    public void run(){
+
         double drawInterval = 1000000000/FPS;
         double delta = 0;
-        long lastTime = System.nanoTime();
+        long lastTime =	System.nanoTime();
         long currentTime;
         long timer = 0;
-        long drawCount = 0;
+        int drawCount = 0;
 
         while(gameThread != null){
             currentTime = System.nanoTime();
+
             delta += (currentTime - lastTime) / drawInterval;
             timer += (currentTime - lastTime);
             lastTime = currentTime;
 
-            if(delta >= 1){
+            if(delta >= 1) {
                 update();
                 repaint();
                 delta--;
                 drawCount++;
             }
-            if(timer >= 1000000000){
-                System.out.println("FPS: " + drawCount);
+            if(timer >= 1000000000) {
+                //System.out.println("FPS:" + drawCount);
                 drawCount = 0;
                 timer = 0;
             }
         }
+
     }
 
-    public void update(){
+    public void update() {
         player.update();
     }
 
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-        Graphics2D g2 = (Graphics2D) g;
+        Graphics2D g2 = (Graphics2D)g;
 
         //TILE
         tileM.draw(g2);
 
         //OBJECT
-        for (int i = 0; i < obj.length; i++) {
-            if (obj[i] != null) {
-                obj[i].draw(g2,this);
+        for(int i = 0; i < obj.length; i++)	{
+            if(obj[i] != null) {
+                obj[i].draw(g2, this);
             }
         }
+
         //PLAYER
         player.draw(g2);
 
+        //UI
+        ui.draw(g2);
+
         g2.dispose();
     }
+    public void playMusic(int i) {
+        music.setFile(i);
+        music.play();
+        music.loop();
+    }
+    public void stopMusic(){
+        music.stop();
+    }
+    public void playSE(int i) {
+        se.setFile(i);
+        se.play();
+    }
 }
+
